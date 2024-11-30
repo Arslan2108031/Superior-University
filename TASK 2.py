493539eeb6721ed7ab06ff7aa8f6aca604bf324c
Arslan2108031 <arsalkhanpilot10@gmail.com>
2024-11-30 14:53:38 +0500

    Update TASK 2.py

--- TASK 2.py
+++ TASK 2.py
@@ -1,4 +1,4 @@
-#Class vs Object
+
 class Car:
     def __init__(self, model, color):
         self.model = model  
@@ -12,11 +12,11 @@ my_car = Car("Toyota", "Red")
 print(f"My car is a {my_car.color} {my_car.model}.")  
 my_car.start()  
 
-# examples of Constructor methods
+
 class Person:
     def __init__(self, name, age):
-        self.name = name  # Initialize the name 
-        self.age = age  # Imitialize the age 
+        self.name = name  
+        self.age = age  
 
     def __str__(self):
         return f"Person: {self.name}, Age: {self.age}" \
